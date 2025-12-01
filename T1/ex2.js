f4401ea13de52d7b5c0e44de0bf893bb011c108e
Mouad Oumous <mouadoumous@gmail.com>
2025-12-01 23:28:41 +0100

    ex 2 utilisation du method filter et map pour filter les membre et extraire les nome

---
+++ T1/ex2.js
@@ -0,0 +1,11 @@
+const personnes = [
+  { nom: 'Rami', age: 33, estMember: true },
+  { nom: 'Fatihi', age: 24, estMember: false },
+  { nom: 'Chakib', age: 45, estMember: true },
+  { nom: 'Mounir', age: 37, estMember: false }
+]
+const nomAges = personnes
+  .filter(personne => personne.estMember === true)
+  .map(personne => personne.nom)
+
+console.log(nomAges)
