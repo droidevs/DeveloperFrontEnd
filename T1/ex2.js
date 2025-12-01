d6eb0a93d5ba23d49d6c44adc986eb00ea67231e
Mouad Oumous <mouadoumous@gmail.com>
2025-12-01 23:30:58 +0100

    fix

--- T1/ex2.js
+++ T1/ex2.js
@@ -3,9 +3,10 @@ const personnes = [
   { nom: 'Fatihi', age: 24, estMember: false },
   { nom: 'Chakib', age: 45, estMember: true },
   { nom: 'Mounir', age: 37, estMember: false }
-]
+];
+
 const nomAges = personnes
   .filter(personne => personne.estMember === true)
-  .map(personne => personne.nom)
+  .map(personne => personne.nom);
 
-console.log(nomAges)
+console.log(nomAges);
